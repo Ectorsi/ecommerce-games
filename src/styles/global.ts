37525e0e9b860aee22b6f9babe773220b0c8752b
José Ector <ector-si@hotmail.com>
2021-07-04 08:12:18 -0300

    breackfast pause

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,6 +7,7 @@ const GlobalStyles = createGlobalStyle`
   font-family: 'Poppins';
   font-style: normal;
   font-weight: 300;
+  font-display: swap;
   src: local(''),
        url('/fonts/poppins-v15-latin-300.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
 
@@ -16,6 +17,7 @@ const GlobalStyles = createGlobalStyle`
   font-family: 'Poppins';
   font-style: normal;
   font-weight: 400;
+  font-display: swap;
   src: local(''),
        url('/fonts/poppins-v15-latin-regular.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
 }
@@ -23,6 +25,7 @@ const GlobalStyles = createGlobalStyle`
   font-family: 'Poppins';
   font-style: normal;
   font-weight: 600;
+  font-display: swap;
   src: local(''),
        url('/fonts/poppins-v15-latin-600.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
 }
@@ -31,6 +34,8 @@ const GlobalStyles = createGlobalStyle`
   margin: 0;
   pad: 0;
   box-sizing: border-box;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 
 html {
@@ -39,7 +44,7 @@ html {
 
 
 body {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
 
 html, body, #__next {
