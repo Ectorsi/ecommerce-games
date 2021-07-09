d82ea26b1154a69b09bfa0167b5cea635cf5317f
José Ector <ector-si@hotmail.com>
2021-07-08 23:32:25 -0300

    making the menu component

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,15 +1,31 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
 
 import Menu from '.';
 
 describe('<Menu /> ', () => {
-  it('Shoud be able to render Menu component', () => {
-    const { container } = render(
+  it('Shoud the render Menu', () => {
+    renderWithTheme(
       <Menu />);
 
-    expect(screen.getByRole('heading', { name: /Menu/i })
-    ).toBeInTheDocument();
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('Shoud handle the open/close mobile menu', () => {
+    renderWithTheme(
+      <Menu />);
+
+    // selecionar o nosso MenuFull
+
+    // verificar se o menu tá escondido
+
+    // clicar no botão de abrir o menu e verificar se ele abriu
+
+    // clicar no botão de fechar o menu e verificar se ele fechou
+
   });
 });
