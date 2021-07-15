7735d07daac33d377cdd9dbc22d1de297b0a5d5e
José Ector <ector-si@hotmail.com>
2021-07-14 21:33:01 -0300

    End of the Highlight component

--- src/components/Highlight/styles.ts
+++ src/components/Highlight/styles.ts
@@ -2,45 +2,72 @@ import styled, { css } from 'styled-components';
 import media from 'styled-media-query';
 import { HighlightProps } from '.';
 
-type WrapperProps = Pick<HighlightProps, 'backgroundImage'>
+type WrapperProps = Pick<HighlightProps, 'backgroundImage' | 'alignment'>
+
+const wrapperModifiers = {
+  right: () => css`
+    grid-template-areas: 'floatimage content';
+    grid-template-columns: 1.3fr 2fr;
+    ${Content} {
+      text-align: right;
+    }
+  `,
+  left: () => css`
+    grid-template-areas: 'content floatimage';
+    grid-template-columns: 2fr 1.3fr;
+    ${Content} {
+      text-align: left;
+    }
+
+    ${FloatImage} {
+      justify-self: end;
+    }
+  `
+}
 
 export const Wrapper = styled.section<WrapperProps>`
-  ${({ backgroundImage }) => css`
+  ${({ backgroundImage, alignment }) => css`
     position: relative;
     height: 23rem;
     display: grid;
+
 
     background-image: url(${backgroundImage});
     background-position: center center;
     background-size: cover;
 
-    ${media.greaterThan('medium')`
-    height: 32rem;
-    `}
 
-    &::before {
+    &::after {
       position: absolute;
       content: '';
       width: 100%;
       height: 100%;
       background-color: rgba(0, 0, 0, 0.6);
     }
+
+    ${media.greaterThan('medium')`
+    height: 32rem;
+    `}
+
+    ${wrapperModifiers[alignment!]()}
   `}
 `;
 
 export const FloatImage = styled.img`
   ${({ theme }) => css`
+    grid-area: floatimage;
     z-index: ${theme.layers.base};
     max-height: 23rem;
     max-width: 100%;
+    align-self: end;
   `}
 `;
 
 export const Content = styled.div`
   ${({ theme }) => css`
+    grid-area: content;
     position: relative;
     z-index: ${theme.layers.base};
-    text-align: right;
     padding: ${theme.spacings.xsmall};
 
     ${media.greaterThan('medium')`
--- src/components/Highlight/test.tsx
+++ src/components/Highlight/test.tsx
@@ -2,7 +2,7 @@ import { screen } from '@testing-library/react';
 import { renderWithTheme } from 'utils/tests/helpers';
 
 import Highlight from '.';
-
+import * as S from './styles';
 const props = {
   title: 'Heading 1',
   subtitle: 'Heading 2',
@@ -30,7 +30,7 @@ describe('<Highlight /> ', () => {
 
     expect(container.firstChild).toHaveStyle({
       backgroundImage: `url(${props.backgroundImage})`
-    })
+    });
   });
 
   it('Shoud be able to render float image', () => {
@@ -39,6 +39,35 @@ describe('<Highlight /> ', () => {
     expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
       'src',
       '/float-image.png'
-    )
+    );
+  });
+
+  it('Shoud be able to render align right by default', () => {
+    const { container } = renderWithTheme(<Highlight {...props} />);
+
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'floatimage content'"
+    );
+
+    expect(container.firstChild).toHaveStyleRule(
+      'text-align',
+      'right',
+      { modifier: `${S.Content}` }
+    );
+  });
+
+  it('Shoud be able to render align left', () => {
+    const { container } = renderWithTheme(<Highlight {...props} alignment="left" />);
+
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'content floatimage'"
+    );
+    expect(container.firstChild).toHaveStyleRule(
+      'text-align',
+      'left',
+      { modifier: `${S.Content}` }
+    );
   });
 });
