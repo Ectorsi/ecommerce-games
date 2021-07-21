9810da85e9363203947615a03214f078acd2cf64
José Ector <ector-si@hotmail.com>
2021-07-20 22:24:19 -0300

    Define mock data in the components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
 import Home from 'templates/Home';
 
-export default function Index() {
-  return <Home />
+export default function Index(props: any) {
+  return <Home {...props} />
+}
+
+export function getServerSideProps() {
+  // Aqui vc faz a lógica
+  // Pode tb buscar dados numa API
+
+  // E depois vc pode retornar os dados
+  return {
+    props: {
+      heading: 'Olha eu aqui!!!! 1234 ssr'
+    }
+  }
 }
