68bcd226898a06b6c3289f3e3189fc3fb2520f2b
José Ector <ector-si@hotmail.com>
2021-07-22 23:36:22 -0300

    Fix and update home template

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -38,48 +38,57 @@ const Home = ({
   <section>
     <Container>
       <Menu />
-      <BannerSlider items={banners} />
+      <S.SectionBanner>
+        <BannerSlider items={banners} />
+      </S.SectionBanner>
     </Container>
 
-    <Container>
-      <Heading lineLeft lineColor="secondary" color="black">
-        News
+    <S.SectionNews>
+      <Container>
+        <Heading lineLeft lineColor="secondary" color="black">
+          News
     </Heading>
 
-      <GameCardSlider items={newGames} color="black" />
-    </Container>
+        <GameCardSlider items={newGames} color="black" />
+      </Container>
+    </S.SectionNews>
 
     <Container>
-      <Heading lineLeft lineColor="secondary">
-        Most Popular
+      <S.SectionMostPopular>
+        <Heading lineLeft lineColor="secondary">
+          Most Popular
     </Heading>
 
-      <Highlight {...mostPopularHighlight} />
-      <GameCardSlider items={mostPopularGames} />
-    </Container>
+        <Highlight {...mostPopularHighlight} />
+        <GameCardSlider items={mostPopularGames} />
+      </S.SectionMostPopular>
 
-    <Container>
-      <Heading lineLeft lineColor="secondary">
-        Upcomming
+
+      <S.SectionUpcoming>
+        <Heading lineLeft lineColor="secondary">
+          Upcomming
     </Heading>
 
-      <GameCardSlider items={upcommingGames} />
-      <Highlight {...upcommingHighlight} />
-      <GameCardSlider items={upcommingMoreGames} />
-    </Container>
+        <GameCardSlider items={upcommingGames} />
+        <Highlight {...upcommingHighlight} />
+        <GameCardSlider items={upcommingMoreGames} />
+      </S.SectionUpcoming>
 
-    <Container>
-      <Heading lineLeft lineColor="secondary">
-        Free Games
+      <S.SectionFreeGames>
+        <Heading lineLeft lineColor="secondary">
+          Free Games
     </Heading>
 
-      <Highlight {...freeHighlight} />
-      <GameCardSlider items={freeGames} />
+        <Highlight {...freeHighlight} />
+        <GameCardSlider items={freeGames} />
+      </S.SectionFreeGames>
     </Container>
 
-    <Container>
-      <Footer />
-    </Container>
+    <S.SectionFooter>
+      <Container>
+        <Footer />
+      </Container>
+    </S.SectionFooter>
   </section>
 )
 
--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -1,3 +1,59 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Wrapper = styled.main``;
+import * as HeadingStyles from 'components/Heading/styles';
+import * as HighlightStyles from 'components/Highlight/styles';
+import * as GameCardSliderStyles from 'components/GameCardSlider/styles';
+import media from 'styled-media-query';
+
+export const Sections = styled.section`
+  ${({ theme }) => css`
+    ${HeadingStyles.Wrapper},
+    ${HighlightStyles.Wrapper},
+    ${GameCardSliderStyles.Wrapper} {
+      margin-bottom: ${theme.spacings.medium};
+    }
+
+    ${HighlightStyles.Wrapper} {
+      ${media.lessThan('medium')`
+        margin-right: calc(-${theme.grid.gutter} / 2);
+        margin-left: calc(-${theme.grid.gutter} / 2);
+      `}
+    }
+
+    ${GameCardSliderStyles.Wrapper} {
+      ${media.lessThan('huge')`
+        margin-right: calc(-${theme.grid.gutter} / 2);
+      `}
+    }
+
+    margin-bottom: calc(${theme.spacings.large} * 2);
+
+  `}
+`;
+
+export const SectionBanner = styled.section`
+  ${({ theme }) => css`
+    margin: ${theme.spacings.large} calc(-${theme.grid.gutter} / 2) ;
+
+    ${media.greaterThan('medium')`
+      margin: ${theme.spacings.large};
+      position: relative;
+      z-index: ${theme.layers.base};
+    `}
+  `}
+`;
+export const SectionNews = styled.div`
+
+`;
+export const SectionMostPopular = styled.div`
+
+`;
+export const SectionUpcoming = styled.div`
+
+`;
+export const SectionFreeGames = styled.div`
+
+`;
+export const SectionFooter = styled.div`
+
+`;
