a8d7f90caf07604a09ac6c12b329603b8820a712
José Ector <ector-si@hotmail.com>
2021-07-29 22:25:16 -0300

    Updating 2 checkbox component

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -8,7 +8,7 @@ export type CheckboxProps = {
 
 const Checkbox = ({ label, labelFor = '', labelColor = 'white' }: CheckboxProps) => (
   <S.Wrapper>
-    <input id={labelFor} type="checkbox" />
+    <S.Input id={labelFor} type="checkbox" />
     {!!label && <S.Label htmlFor={labelFor} labelColor={labelColor} >{label}</S.Label>}
   </S.Wrapper>
 );
