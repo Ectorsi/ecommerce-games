cc2fbfda7914058855346b966f6bc97e78b143e5
José Ector Oliveira de Souza <p22.jose@webmotors.com.br>
2021-09-24 00:08:00 -0300

    ending radio component

--- src/components/Radio/index.tsx
+++ src/components/Radio/index.tsx
@@ -4,41 +4,41 @@ import * as S from './styles';
 type RadioValue = string | ReadonlyArray<string> | number;
 
 export type RadioProps = {
-  onCheck?: (value?: RadioValue) => void;
-  labelColor?: 'white' | 'black';
-  label?: string;
-  labelFor?: string;
-  value?: RadioValue
+    onCheck?: (value?: RadioValue) => void;
+    labelColor?: 'white' | 'black';
+    label?: string;
+    labelFor?: string;
+    value?: RadioValue
 } & InputHTMLAttributes<HTMLInputElement>
 
 const Radio = ({
-  onCheck,
-  labelColor = 'white',
-  label,
-  labelFor = '',
-  value,
-  ...props
+    onCheck,
+    labelColor = 'white',
+    label,
+    labelFor = '',
+    value,
+    ...props
 }: RadioProps) => {
 
-  const onChange = () => {
-    !!onCheck && onCheck(value)
-  }
+    const onChange = () => {
+        !!onCheck && onCheck(value)
+    }
 
-  return (
-    <S.Wrapper>
-      <S.Input
-        id={labelFor}
-        type="radio"
-        value={value}
-        onChange={onChange}
-        {...props}
-      />
-      {!!label && (
-        <S.Label htmlFor={labelFor} labelColor={labelColor}>
-          {label}
-        </S.Label>)}
-    </S.Wrapper>
-  );
+    return (
+        <S.Wrapper>
+            <S.Input
+                id={labelFor}
+                type="radio"
+                value={value}
+                onChange={onChange}
+                {...props}
+            />
+            {!!label && (
+                <S.Label htmlFor={labelFor} labelColor={labelColor}>
+                    {label}
+                </S.Label>)}
+        </S.Wrapper>
+    );
 }
 
 export default Radio;
--- src/components/Radio/test.tsx
+++ src/components/Radio/test.tsx
@@ -1,15 +1,60 @@
-import { render, screen } from '@testing-library/react';
-
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { renderWithTheme } from 'utils/tests/helpers';
 import Radio from '.';
 
 describe('<Radio /> ', () => {
-  it('Shoud be able to render Radio component', () => {
-    const { container } = render(
-      <Radio />);
+    it('Shoud be able to render Radio component with label', () => {
+        const { container } = renderWithTheme(<Radio label="RadioButton label" labelFor="radio" />);
 
-    expect(screen.getByRole('heading', { name: /Radio/i })
-    ).toBeInTheDocument();
+        expect(screen.getByRole('radio')).toBeInTheDocument();
+        expect(screen.getByLabelText(/radioButton label/i)).toBeInTheDocument();
+        expect(screen.getByText(/radio/i)).toHaveAttribute(
+            'for',
+            'radio',
+        );
 
-    expect(container.firstChild).toMatchSnapshot();
-  });
+        expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it('Shoud be able to render Radio component without label', () => {
+        renderWithTheme(<Radio />);
+        expect(screen.queryByLabelText(/radioButton label/i)).not.toBeInTheDocument();
+    });
+
+    it('Shoud be able to render Radio component with black label', () => {
+        renderWithTheme(<Radio label="RadioButton label" labelFor="radio" labelColor="black" />);
+        expect(screen.getByText(/radioButton label/i)).toHaveStyle({
+            color: '#030517'
+        });
+    });
+
+    it('Shoud dispath onChange when label status changes', async () => {
+        const onCheck = jest.fn();
+        renderWithTheme(
+            <Radio
+                label="RadioButton label"
+                onCheck={onCheck}
+            />
+        );
+        expect(onCheck).not.toHaveBeenCalled();
+        userEvent.click(screen.getByRole('radio'));
+        await waitFor(() => {
+            expect(onCheck).toHaveBeenCalledTimes(1);
+        });
+    });
+
+    it('should be able acessible with tab', () => {
+        renderWithTheme(
+            <Radio
+                label="RadioButton label"
+                labelFor="radio"
+            />
+        );
+
+        expect(document.body).toHaveFocus();
+        userEvent.tab();
+        expect(screen.getByLabelText(/radioButton label/i)).toHaveFocus();
+
+    });
 });
