d24cfd0ef4db0a341fa6100412fee5bc8b5015ab
José Ector Oliveira de Souza <p22.jose@webmotors.com.br>
2021-09-29 16:37:05 -0300

    codeing Auth component

--- src/templates/Auth/styles.ts
+++ src/templates/Auth/styles.ts
@@ -1,11 +1,44 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Wrapper = styled.main``;
+export const Wrapper = styled.main`
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        height: 100vh;
+`;
 
-export const BannerBlock = styled.div``;
+export const BannerBlock = styled.div`
+    ${({ theme }) => css`
+        background-image: url(/img/auth-bg.jpg);
+        background-size: cover;
+        background-position: center center;
+        position: relative;
+
+        &:after {
+            content: '';
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            background: ${theme.colors.black};
+            opacity: 0.85;
+        }
+    `}
+`;
+
+export const BannerContent = styled.div`
+    ${({ theme }) => css`
+        position: relative;
+        z-index: ${theme.layers.base};
+    `}
+`;
 
 export const Subtitle = styled.h3``;
 
 export const Footer = styled.p``;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+    ${({ theme }) => css`
+        background: ${theme.colors.white};
+    `}
+`;
