12166fd70b6fa2aff766662ffc4a567a41d4ef12
José Ector Oliveira de Souza <p22.jose@webmotors.com.br>
2021-10-21 00:28:42 -0300

    fix button component, Menu component and make ending showcase component tests

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -4,55 +4,54 @@ import { renderWithTheme } from 'utils/tests/helpers';
 import Menu from '.';
 
 describe('<Menu /> ', () => {
-  it('Shoud the render Menu', () => {
-    renderWithTheme(
-      <Menu />);
+    it('Shoud the render Menu', () => {
+        renderWithTheme(
+            <Menu />);
 
-    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
-    expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
-    expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
-    expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
+        expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument();
+        expect(screen.getByLabelText(/search/i)).toBeInTheDocument();
+        expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument();
+        expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument();
 
-  });
+    });
 
-  it('Shoud handle the open/close mobile menu', () => {
-    renderWithTheme(
-      <Menu />);
+    it('Shoud handle the open/close mobile menu', () => {
+        renderWithTheme(<Menu />);
 
-    // selecionar o nosso MenuFull
-    const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+        // selecionar o nosso MenuFull
+        const fullMenuElement = screen.getByRole('navigation', { hidden: true });
 
-    // verificar se o menu tá escondido
-    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
-    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
-    // clicar no botão de abrir o menu e verificar se ele abriu
-    fireEvent.click(screen.getByLabelText(/open menu/i))
-    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
-    expect(fullMenuElement).toHaveStyle({ opacity: 1 })
-    // clicar no botão de fechar o menu e verificar se ele fechou
-    fireEvent.click(screen.getByLabelText(/close menu/i))
-    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
-    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+        // verificar se o menu tá escondido
+        expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true');
+        expect(fullMenuElement).toHaveStyle({ opacity: 0 });
+        // clicar no botão de abrir o menu e verificar se ele abriu
+        fireEvent.click(screen.getByLabelText(/open menu/i));
+        expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false');
+        expect(fullMenuElement).toHaveStyle({ opacity: 1 });
+        // clicar no botão de fechar o menu e verificar se ele fechou
+        fireEvent.click(screen.getByLabelText(/close menu/i));
+        expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true');
+        expect(fullMenuElement).toHaveStyle({ opacity: 0 });
 
-  });
+    });
 
-  it('Should show resgister box when logged out', () => {
-    renderWithTheme(<Menu />)
+    it('Should show resgister box when logged out', () => {
+        renderWithTheme(<Menu />)
 
-    expect(screen.queryByText(/my account/i)).not.toBeInTheDocument()
-    expect(screen.queryByText(/Wishlist/i)).not.toBeInTheDocument()
+        expect(screen.queryByText(/my account/i)).not.toBeInTheDocument();
+        expect(screen.queryByText(/Wishlist/i)).not.toBeInTheDocument();
 
-    expect(screen.getByText(/log in now/i)).toBeInTheDocument()
-    expect(screen.getByText(/sign up/i)).toBeInTheDocument()
-  })
+        expect(screen.getAllByText(/sign in/i)).toHaveLength(2);
+        expect(screen.getByText(/sign up/i)).toBeInTheDocument();
+    })
 
-  it('Should not show resgister box when logged in', () => {
-    renderWithTheme(<Menu username="Ector" />)
+    it('should show wishlight and account when logged in', () => {
+        renderWithTheme(<Menu username="Ector" />)
 
-    expect(screen.getByText(/my account/i)).toBeInTheDocument()
-    expect(screen.getByText(/Wishlist/i)).toBeInTheDocument()
+        expect(screen.getByText(/my account/i)).toBeInTheDocument()
+        expect(screen.getByText(/Wishlist/i)).toBeInTheDocument()
 
-    expect(screen.queryByText(/log in now/i)).not.toBeInTheDocument()
-    expect(screen.queryByText(/sign up/i)).not.toBeInTheDocument()
-  })
+        expect(screen.queryByText(/sign in/i)).not.toBeInTheDocument()
+        expect(screen.queryByText(/sign up/i)).not.toBeInTheDocument()
+    })
 });
--- src/components/Showcase/test.tsx
+++ src/components/Showcase/test.tsx
@@ -15,13 +15,37 @@ const props = {
 
 describe('<Showcase /> ', () => {
     it('Shoud be able to render Showcase component as default', () => {
-        renderWithTheme(<Showcase {...props} />);
+        const { container } = renderWithTheme(<Showcase {...props} />);
 
         expect(screen.getByText(/Most Popular/i)).toBeInTheDocument();
         expect(screen.getByRole('heading', { name: highlightMock.title })).toBeInTheDocument();
         expect(screen.getByRole('heading', { name: gamesMock[0].title })).toBeInTheDocument();
 
 
-        // expect(container.firstChild).toMatchSnapshot();
+        expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it('Shoud be able to render Showcase component without title', () => {
+        renderWithTheme(<Showcase games={props.games} highlight={props.highlight} />);
+
+        expect(screen.getByRole('heading', { name: gamesMock[0].title })).toBeInTheDocument();
+        expect(screen.getByRole('heading', { name: highlightMock.title })).toBeInTheDocument();
+        expect(screen.queryByText(/Most Popular/i)).not.toBeInTheDocument();
+    });
+
+    it('Shoud be able to render Showcase component without highlight', () => {
+        renderWithTheme(<Showcase games={props.games} title={props.title} />);
+
+        expect(screen.getByRole('heading', { name: gamesMock[0].title })).toBeInTheDocument();
+        expect(screen.queryByRole('heading', { name: highlightMock.title })).not.toBeInTheDocument();
+        expect(screen.getByText(/Most Popular/i)).toBeInTheDocument();
+    });
+
+    it('Shoud be able to render Showcase component without games', () => {
+        renderWithTheme(<Showcase title={props.title} highlight={props.highlight} />);
+
+        expect(screen.queryByRole('heading', { name: gamesMock[0].title })).not.toBeInTheDocument();
+        expect(screen.getByRole('heading', { name: highlightMock.title })).toBeInTheDocument();
+        expect(screen.getByText(/Most Popular/i)).toBeInTheDocument();
     });
 });
