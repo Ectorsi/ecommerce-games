57c6c6390d6d5e94de6e7ad2e4f5ce2105420062
José Ector Oliveira de Souza <p22.jose@webmotors.com.br>
2021-11-15 20:25:43 -0300

    make GameInfo component

--- src/components/Showcase/styles.ts
+++ src/components/Showcase/styles.ts
@@ -12,20 +12,20 @@ export const Wrapper = styled(Container).attrs({ as: 'section' })`
         ${HeadingStyles.Wrapper},
         ${HighlightStyles.Wrapper},
         ${GameCardSliderStyles.Wrapper} {
-        margin-bottom: ${theme.spacings.medium};
+            margin-bottom: ${theme.spacings.medium};
         }
 
         ${HighlightStyles.Wrapper} {
-        ${media.lessThan('medium')`
-            margin-right: calc(-${theme.grid.gutter} / 2);
-            margin-left: calc(-${theme.grid.gutter} / 2);
-        `}
+            ${media.lessThan('medium')`
+                margin-right: calc(-${theme.grid.gutter} / 2);
+                margin-left: calc(-${theme.grid.gutter} / 2);
+            `}
         }
 
         ${GameCardSliderStyles.Wrapper} {
-        ${media.lessThan('huge')`
-            margin-right: calc(-${theme.grid.gutter} / 2);
-        `}
+            ${media.lessThan('huge')`
+                margin-right: calc(-${theme.grid.gutter} / 2);
+            `}
         }
 
         margin-bottom: calc(${theme.spacings.large} * 2);
